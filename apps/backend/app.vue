c1872994458cee60335fdab6f073c4268231a9d3
Larry Williamson <l422y@l422y.com>
2024-06-29 14:14:42 -0400

    initial commit

---
+++ apps/backend/app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    {{ envFile }} / {{ env}}
+  </div>
+</template>
+<script lang="ts" setup>
+const config = useRuntimeConfig().public
+const { envFile, env } = config
+</script>
